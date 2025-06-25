19906f9faaadb625945f3ec2a6a74db6134884f5
TheRealKuchen <111386628+TheRealKuchen@users.noreply.github.com>
2025-06-26 00:20:45 +0200

    Add files via upload

---
+++ INSTAL/ox_inventory.lua
@@ -0,0 +1,56 @@
+return {
+    ['shovel'] = {
+        label = 'Shovel',
+        weight = 1000,
+        stack = false,
+        close = true,
+        client = {
+            export = 'sar_digging.useShovel'
+        }
+    },
+    ['goldnugget'] = {
+        label = 'Gold Nugget',
+        weight = 3,
+        stack = true,
+    },
+    ['md_rustynails'] = {
+        label = 'Rusty Nails',
+        weight = 2,
+        stack = true,
+    },
+    ['md_crushedcan'] = {
+        label = 'Old Can',
+        weight = 2,
+        stack = true,
+    },
+    ['md_rustyspring'] = {
+        label = 'Rusty Spring',
+        weight = 10,
+        stack = true,
+    },
+    ['paperclip_rusty'] = {
+        label = 'Rusty Paperclip',
+        weight = 1,
+        stack = true,
+    },
+    ['md_ancientcoin'] = {
+        label = 'Ancient Coin',
+        weight = 15,
+        stack = true,
+    },
+    ['md_bottlecap'] = {
+        label = 'Bottle Cap',
+        weight = 15,
+        stack = true,
+    },
+    ['floppydisk_rusty'] = {
+        label = 'Rusty Floppy Disk',
+        weight = 15,
+        stack = true,
+    },
+    ['md_rustyring'] = {
+		label = 'Rusty ring',
+		weight = 20,
+		stack = true,
+	},
+}
